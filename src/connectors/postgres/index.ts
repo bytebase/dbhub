53722e1d1d7712d278c0a230843dd8f09ecabcb9
theboringhumane <me@iamharsh.dev>
2025-05-04 09:42:10 +0530

    fix: refine Postgres SSL configuration handling
    - Improved logic for setting SSL configuration based on the sslmode query parameter.
    - Ensured that the default behavior for NODE_TLS_REJECT_UNAUTHORIZED is to enforce SSL certificate validation.
    - Cleaned up redundant code related to SSL settings.

--- src/connectors/postgres/index.ts
+++ src/connectors/postgres/index.ts
@@ -38,29 +38,33 @@ class PostgresDSNParser implements DSNParser {
       // Handle query parameters (like sslmode, etc.)
       url.searchParams.forEach((value, key) => {
         if (key === "sslmode") {
-          config.ssl = value !== "disable";
+          if (value === "disable") {
+            config.ssl = false;
+          } else {
+            // Allow for env variable to override sslmode
+            // NODE_TLS_REJECT_UNAUTHORIZED=0 disables SSL certificate validation
+            // NODE_TLS_REJECT_UNAUTHORIZED=1 enforces SSL certificate validation
+            // Default to 1 to maintain security
+
+            let {
+              NODE_TLS_REJECT_UNAUTHORIZED,
+            } = process.env;
+
+            if (NODE_TLS_REJECT_UNAUTHORIZED === undefined) {
+              NODE_TLS_REJECT_UNAUTHORIZED = "1"; // Default to enforcing SSL cert validation
+            }
+
+            // Configure SSL settings based on env var
+            // rejectUnauthorized: true = Validate SSL cert
+            // rejectUnauthorized: false = Allow self-signed/invalid certs
+            config.ssl = {
+              rejectUnauthorized: JSON.parse(NODE_TLS_REJECT_UNAUTHORIZED) == true,
+            };
+          }
         }
         // Add other parameters as needed
       });
 
-      // Allow for env variable to override sslmode
-      // NODE_TLS_REJECT_UNAUTHORIZED=0 disables SSL certificate validation
-      // NODE_TLS_REJECT_UNAUTHORIZED=1 enforces SSL certificate validation
-      // Default to 1 to main security
-      let {
-        NODE_TLS_REJECT_UNAUTHORIZED,
-      } = process.env;
-
-      if (!NODE_TLS_REJECT_UNAUTHORIZED) {
-        NODE_TLS_REJECT_UNAUTHORIZED = "1"; // Default to disallowing self-signed certs
-      }
-
-      // Configure SSL settings based on env var
-      // rejectUnauthorized: true = Validate SSL cert
-      // rejectUnauthorized: false = Allow self-signed/invalid certs
-      config.ssl = {
-        rejectUnauthorized: JSON.parse(NODE_TLS_REJECT_UNAUTHORIZED) == true,
-      };
 
       return config;
     } catch (error) {
